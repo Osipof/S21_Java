e92a5033eb1f3dfd33276c0b5cd6c559776a6922
Ivan Osipov <102904342+osipof@users.noreply.github.com>
2023-04-25 21:02:15 +0300

    Day 05 - add all files and structure. Need to fill *.sql files

---
+++ java_bootcamp/day05/ex00/Chat/src/main/java/edu/school21/chat/Chatroom.java
@@ -0,0 +1,73 @@
+package edu.school21.chat;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Chatroom {
+    private int id;
+    private String name;
+    private String owner;
+    List<Message> messages;
+
+    public Chatroom(int id, String name, String owner, List<Message> messages) {
+        this.id = id;
+        this.name = name;
+        this.owner = owner;
+        this.messages = messages;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Chatroom chatroom = (Chatroom) o;
+        return id == chatroom.id && Objects.equals(name, chatroom.name) && Objects.equals(owner, chatroom.owner) && Objects.equals(messages, chatroom.messages);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, owner, messages);
+    }
+
+    @Override
+    public String toString() {
+        return "Chatroom{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", owner='" + owner + '\'' +
+                ", messages=" + messages +
+                '}';
+    }
+}
