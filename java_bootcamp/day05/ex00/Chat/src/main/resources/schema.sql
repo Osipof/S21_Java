6a3682275af8b550411a10ac93c1728a800d5e7f
Ivan Osipov <102904342+osipof@users.noreply.github.com>
2023-05-30 21:54:08 +0300

    day05:ex00: Add data in data.sql and schema.sql

--- java_bootcamp/day05/ex00/Chat/src/main/resources/schema.sql
+++ java_bootcamp/day05/ex00/Chat/src/main/resources/schema.sql
@@ -1,34 +1,27 @@
 drop schema if exists chat cascade;
+drop table if exists chat.user, chat.message, chat.chatroom, chat.user_chatroom;
 
 create schema if not exists chat;
 
-create table if not exists chat.user (
-    id          serial primary key,
-    name        varchar(20) not null unique,
-    password    varchar(20) not null
+create table if not exists chat.user
+(
+    id       serial primary key,
+    name     varchar(20) not null unique,
+    password varchar(20) not null
 );
 
-create table if not exists chat.chatroom (
-    id          serial primary key,
-    title       varchar(20) not null unique,
-    owner       integer not null
-    foreign key (owner) references chat.user(id)
+create table if not exists chat.chatroom
+(
+    id         serial primary key,
+    chat_name  varchar(20) not null unique,
+    chat_owner integer     not null references chat.user (id)
 );
 
-create table if not exists chat.message (
-    id          serial primary key,
-    author      integer not null,
-    room        integer not null,
-    text        text not null,
-    lDateTime   timestamp default CURRENT_TIMESTAMP,
-    foreign key (author) references chat.user(id),
-    foreign key (room) references chat.chatroom(id)
-);
-
-create table if not exists chat.user_chatroom (
-    id          serial primary key,
-    user_id     integer not null,
-    chat_id     integer not null,
-    foreign key (user_id) references chat.user(id),
-    foreign key (chat_id) references chat.chatroom(id)
+create table if not exists chat.message
+(
+    id        serial primary key,
+    sender_id integer not null references chat.user (id),
+    room_id   integer not null references chat.chatroom (id),
+    text      text    not null,
+    lDateTime timestamp default CURRENT_TIMESTAMP
 );
