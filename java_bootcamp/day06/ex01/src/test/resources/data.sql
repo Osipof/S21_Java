9f7d0ac89cf0307f011898ed96cc4561c9c3546e
Ivan Osipov <102904342+osipof@users.noreply.github.com>
2023-07-02 18:45:59 +0300

    day06: ex01: Complete

---
+++ java_bootcamp/day06/ex01/src/test/resources/data.sql
@@ -0,0 +1,6 @@
+insert into tests.product (identifier, name, price)
+values (1, 'test_name', 123),
+       (2, 'test_two', 80),
+       (3, 'banana', 99),
+       (4, 'iphone 13', 65000),
+       (5, 'mac book pro', 180000);
